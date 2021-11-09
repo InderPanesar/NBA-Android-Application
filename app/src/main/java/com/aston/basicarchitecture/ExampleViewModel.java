3d2b1e25f4db91ed81eae1060316f8510528f79a
Inder Panesar <Inder_Panesar@hotmail.com>
2021-11-09 01:46:07 +0000

    Simple LiveData + ViewModel Implementation For MVVM

---
+++ app/src/main/java/com/aston/basicarchitecture/ExampleViewModel.java
@@ -0,0 +1,60 @@
+package com.aston.basicarchitecture;
+
+import android.util.Log;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import com.aston.basicarchitecture.engine.model.ExampleModel;
+import com.aston.basicarchitecture.engine.repository.ExampleRepository;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Named;
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+@HiltViewModel
+public class ExampleViewModel extends ViewModel {
+
+    ExampleRepository repository;
+    @Inject
+    ExampleViewModel(@Named("ExampleRepository") ExampleRepository exampleRepository) {
+        repository = exampleRepository;
+    }
+
+    LiveData<String> getList() {
+        MutableLiveData<String> data = new MutableLiveData<>();
+        Log.d("HIT", "GOT LIST");
+        repository.getPosts().enqueue(new Callback<List<ExampleModel>>() {
+            @Override
+            public void onResponse(Call<List<ExampleModel>> call, Response<List<ExampleModel>> response) {
+              if(!response.isSuccessful()) {
+                  Log.d("1", "HIT");
+                  //textViewResult.setText("Code: " + response.code());
+                  data.postValue("Code: " + response.code());
+                  return;
+              }
+              List<ExampleModel> models = response.body();
+                String content = "";
+                for(ExampleModel model : models) {
+                    content += "ID: " + model.getId() + "\n";
+                    content += "User ID: " + model.getUserId() + "\n";
+                    content += "Title: " + model.getTitle() + "\n";
+                    content += "Text: " + model.getText() + "\n\n";
+              }
+                data.postValue(content);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<ExampleModel>> call, Throwable t) {
+                Log.d("3", "HIT");
+                data.postValue("Error:" + t.getMessage());
+
+            }
+        });
+        return data;
+
+    }
+}
--- app/src/main/java/com/aston/basicarchitecture/MainActivity.java
+++ app/src/main/java/com/aston/basicarchitecture/MainActivity.java
@@ -1,66 +1,36 @@
 package com.aston.basicarchitecture;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
-
-import com.aston.basicarchitecture.engine.comms.api.ExampleAPIModule;
-import com.aston.basicarchitecture.engine.model.ExampleModel;
-import com.aston.basicarchitecture.engine.repository.ExampleRepository;
-import com.aston.basicarchitecture.engine.repository.ExampleRepositoryImpl;
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-
+import androidx.lifecycle.Observer;
 import dagger.hilt.android.AndroidEntryPoint;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
-    @Inject
-    @Named(value="ExampleRepository")
-    ExampleRepository repository;
+    private ExampleViewModel exampleViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewResult = findViewById(R.id.text_view_result);
+        exampleViewModel = new ViewModelProvider(this).get(ExampleViewModel.class);
 
-
-        repository.getPosts().enqueue(new Callback<List<ExampleModel>>() {
-                @Override
-                public void onResponse(Call<List<ExampleModel>> call, Response<List<ExampleModel>> response) {
-                    if(!response.isSuccessful()) {
-                        textViewResult.setText("Code: " + response.code());
-                        return;
-                    }
-                    List<ExampleModel> models = response.body();
-                    for(ExampleModel model : models) {
-                        String content = "";
-                        content += "ID: " + model.getId() + "\n";
-                        content += "User ID: " + model.getUserId() + "\n";
-                        content += "Title: " + model.getTitle() + "\n";
-                        content += "Text: " + model.getText() + "\n\n";
-                        textViewResult.append(content);
-                    }
-
-                }
-
-                @Override
-                public void onFailure(Call<List<ExampleModel>> call, Throwable t) {
-                    textViewResult.setText(t.getMessage());
-                }
+        Observer<String> nameObserver = new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable final String newName) {
+                textViewResult.setText(newName);
             }
-        );
+        };
 
-
+        exampleViewModel.getList().observe(this, nameObserver);
+        
     }
 }
