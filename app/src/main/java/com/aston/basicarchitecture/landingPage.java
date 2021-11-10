a926cb02e79ada27c9fb6248a7a6a13b63373d8b
Inder Panesar <Inder_Panesar@hotmail.com>
2021-11-10 02:37:28 +0000

    Remove temporary code

--- app/src/main/java/com/aston/basicarchitecture/landingPage.java
+++ app/src/main/java/com/aston/basicarchitecture/landingPage.java
@@ -1,7 +1,6 @@
 package com.aston.basicarchitecture;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +8,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 
 /**
@@ -25,47 +22,20 @@ import android.widget.Toolbar;
  */
 public class landingPage extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private TextView textViewResult;
     private ExampleViewModel exampleViewModel;
 
-    public landingPage() {
-        // Required empty public constructor
-    }
+    public landingPage() { }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment landingPage.
-     */
-    // TODO: Rename and change types and number of parameters
     public static landingPage newInstance(String param1, String param2) {
         landingPage fragment = new landingPage();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -83,19 +53,25 @@ public class landingPage extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_landing_page, container, false);
 
+        //Get Text Area
         textViewResult = (TextView) v.findViewById(R.id.textView1);
-        v.findViewById(R.id.textView1).setOnClickListener(new View.OnClickListener() {
+
+        //When Clicked Move to next fragment.
+        textViewResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Navigation.findNavController(view).navigate(R.id.action_landingPage_to_mainpage2);
             }
         });
 
+        //Get Activities.
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(false);
 
+        //Set the ViewModel
         exampleViewModel = new ViewModelProvider(getActivity()).get(ExampleViewModel.class);
 
+        //Observer
         Observer<String> nameObserver = new Observer<String>() {
             @Override
             public void onChanged(@Nullable final String newName) {
@@ -103,6 +79,7 @@ public class landingPage extends Fragment {
             }
         };
 
+        //Set TextView with a list of api requests.
         exampleViewModel.getList().observe(getViewLifecycleOwner(), nameObserver);
 
         return v;
