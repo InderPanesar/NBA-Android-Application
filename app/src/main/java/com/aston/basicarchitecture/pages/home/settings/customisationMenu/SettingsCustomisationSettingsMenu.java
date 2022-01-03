fb31bfe8fdfa5db75a47663542ce1c93b0e6d766
Inder Panesar <Inder_Panesar@hotmail.com>
2022-01-03 06:21:56 +0000

    Fix Error Exception, and add UI implementation for settings

--- app/src/main/java/com/aston/basicarchitecture/pages/home/settings/customisationMenu/SettingsCustomisationSettingsMenu.java
+++ app/src/main/java/com/aston/basicarchitecture/pages/home/settings/customisationMenu/SettingsCustomisationSettingsMenu.java
@@ -3,12 +3,21 @@ package com.aston.basicarchitecture.pages.home.settings.customisationMenu;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.aston.basicarchitecture.R;
+import com.aston.basicarchitecture.pages.home.settings.favouriteTeam.SettingsTeamPreferenceViewModel;
+import com.google.android.material.slider.Slider;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +26,10 @@ import com.aston.basicarchitecture.R;
  */
 public class SettingsCustomisationSettingsMenu extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    SettingsTeamPreferenceViewModel viewModel;
+    List<SwitchMaterial> sliderList = new ArrayList<>();
+    List<SwitchMaterial> activeSwitches = new ArrayList<>();
 
     public SettingsCustomisationSettingsMenu() {
         // Required empty public constructor
@@ -34,35 +39,69 @@ public class SettingsCustomisationSettingsMenu extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment settingsCustomisationSettingsMenu.
      */
     // TODO: Rename and change types and number of parameters
-    public static SettingsCustomisationSettingsMenu newInstance(String param1, String param2) {
+    public static SettingsCustomisationSettingsMenu newInstance() {
         SettingsCustomisationSettingsMenu fragment = new SettingsCustomisationSettingsMenu();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment.
         View v = inflater.inflate(R.layout.fragment_settings_customisation_settings_menu, container, false);
+        viewModel = new ViewModelProvider(this.getActivity()).get(SettingsTeamPreferenceViewModel.class);
+
+        // Adds sliders to the view.
+        sliderList.add(v.findViewById(R.id.pointsPerGameSlider));
+        sliderList.add(v.findViewById(R.id.assistsPerGameSlider));
+        sliderList.add(v.findViewById(R.id.reboundsPerGameSlider));
+        sliderList.add(v.findViewById(R.id.overallFieldGoalPercentageSlider));
+        sliderList.add(v.findViewById(R.id.threePointFieldGoalPercentageSlider));
+        sliderList.add(v.findViewById(R.id.twoPointFieldGoalPercentageSlider));
+        sliderList.add(v.findViewById(R.id.freeThrowPercentageSlider));
+        sliderList.add(v.findViewById(R.id.freeThrowsMadeSlider));
+        sliderList.add(v.findViewById(R.id.plusMinusSlider));
+
+        for(SwitchMaterial _switch : sliderList) {
+            _switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked) { activeSwitches.add(_switch); }
+                    else { activeSwitches.remove(_switch); }
+                    disableAllSwitches(activeSwitches.size());
+                }
+
+            });
+        }
+
+
 
         return v;
+    }
+
+    private void disableAllSwitches (int switchesActive) {
+        if(switchesActive == 4) {
+            for(SwitchMaterial s : sliderList) {
+                if(!activeSwitches.contains(s)) {
+                    s.setEnabled(false);
+                }
+            }
+        }
+        else {
+            for(SwitchMaterial s : sliderList) {
+                if(!s.isEnabled()) {
+                    s.setEnabled(true);
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/aston/basicarchitecture/pages/home/settings/customisationMenu/SettingsCustomisationSettingsMenuViewModel.java
@@ -0,0 +1,6 @@
+package com.aston.basicarchitecture.pages.home.settings.customisationMenu;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingsCustomisationSettingsMenuViewModel extends ViewModel {
+}
